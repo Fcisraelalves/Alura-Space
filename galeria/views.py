0db7ddb8a3b3ebbc136ac34a87e289e9967f8a2b
Fcisraelalves <alves.israel@aluno.ifce.edu.br>
2026-02-16 16:05:59 -0300

    feat: correções para permitir a busca de imagens no banco de dados
    Primeiramente, foi adicionar a seguinte linha na view do index:
    -   **fotos = Fotografia.objects.all()**
    Basicamente, o que ela faz é pegar todos os objetos Fotografia salvos dentro do banco e os retorna na forma de uma lista de models, que poderá ser passada no retorno da função, uma vez que esse aceita o parâmetro opcional **'context'**, que é um dicionário com variáveis que podem ser usadas no template.
    
    Além das mudanças nas views, fizeram-se necessárias mudanças no index.html, adicionando uma estrutura de repetição que iria percorrer a lista de models e acessar cada um dos seus atributos individualmente, tudo possibilitado pela DTL (*Django Template Language*).
    
    Por último, adicionei uma imagem para fins de testes das mudanças realizadas no sistema.

--- galeria/views.py
+++ galeria/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Fotografia
 # Create your views here.
 def index(request):
-    return render(request, 'galeria/index.html')
+    fotos = Fotografia.objects.all()
+    return render(request, 'galeria/index.html', {'fotos' : fotos})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
