23b4062d26acb84c7b04863c10ac390cf1e76cd3
Fcisraelalves <alves.israel@aluno.ifce.edu.br>
2026-02-16 22:40:09 -0300

    feat: adicionar a coluna 'categoria' ao model
    Para fins de implementação de filtros futuros, achei conveniente a adição do campo 'categoria', utilizei um TextChoices, pois esse campo tem apenas 4 opções possíveis por enquanto.

--- galeria/models.py
+++ galeria/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Categorias(models.TextChoices):
+    GALAXIA = 'GALAXIA', 'Galáxia'
+    ESTRELA = 'ESTRELA', 'Estrela'
+    PLANETA = 'PLANETA', 'Planeta'
+    NEBULOSA = 'NEBULOSA', 'Nebulosa'
+
 class Fotografia(models.Model):
     nome = models.CharField(max_length=100, null=False, blank=False)
+    categoria = models.CharField(max_length=100, choices=Categorias.choices, default=Categorias.GALAXIA)
     legenda = models.CharField(max_length=150, null=False, blank=False)
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
 
     def __str__(self):
         return f'Fotografia [nome={self.nome}]'
-    
     
