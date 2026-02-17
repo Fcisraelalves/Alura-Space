491a4a8efd1d2d95e9b0b132b96413ecb9f00d95
Fcisraelalves <alves.israel@aluno.ifce.edu.br>
2026-02-16 23:52:39 -0300

    feat: adicionar paginação e agluns filtros no painel administrativo do Django
    Adicionei 'publicada' ao **list_display**, permitindo que essa informação apareça ao listar as fotografias e inclui 'categoria' no novo atributo **list_filter**, o que possibilita filtrar fotografias pelo status delas.
    
    Além disso, adicionei a paginação por meio do atributo **list_per_page** e tornei o campo 'publicada' editável por meio da própria listagem de fotografias (sem precisar clicar na fotografia em questão) utilizando o atributo **list_editable**.

--- galeria/admin.py
+++ galeria/admin.py
@@ -3,8 +3,11 @@ from .models import Fotografia
 # Register your models here.
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'legenda')
+    list_display = ('id', 'nome', 'legenda', 'publicada')
     list_display_links = ['nome']
     search_fields = ['nome']
+    list_filter = ['categoria']
+    list_editable = ['publicada']
+    list_per_page = 10
 
 admin.site.register(Fotografia, ListandoFotografias)
