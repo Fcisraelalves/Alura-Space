f18725208681d2e5e52af5ea104f835410949475
Fcisraelalves <alves.israel@aluno.ifce.edu.br>
2026-02-16 22:27:38 -0300

    feat: personalizando o painel administrativo do Django
    Basicamente, criei a classe **ListandoFotografias**, que permite a personalização da exibição das fotografias cadastradas no sistema, a seguir o que cada um dos campos adicionados faz:
    
    - **list_display:** os campos que serão exibidos na listagem das fotografias.
    - **list_display_links:** os campos que permitem acessar os detalhes de cada uma das fotografias.
    - **search_fields:** os campos que podem ser utilizados pelo motor de busca para filtrar fotografias.

--- galeria/admin.py
+++ galeria/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Fotografia
 # Register your models here.
 
-admin.site.register(Fotografia)
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'legenda')
+    list_display_links = ['nome']
+    search_fields = ['nome']
+
+admin.site.register(Fotografia, ListandoFotografias)
